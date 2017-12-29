ff0c982b404f15e89c117544f3fe01c8dfcc68db
Amlan Mukherjee <34948059+Amlanmukherjee@users.noreply.github.com>
2017-12-30 03:25:17 +0530

    Add files via upload

--- list_dict.py
+++ list_dict.py
@@ -1,18 +1,18 @@
-num=input('enter number of elements you want to enter')
-a=[]
-b=[]
-c={}
-d=int(num)
+num_1=input('enter number of elements you want to enter')
+list_1=[]
+list_2=[]
+dict_1={}
+num_2=int(num_1)
 print('enter element for list 1:::')
-for j in range(0,d):
-    e=input('enter element')
-    a.append(e)
+for j in range(0,num_2):
+    input_1=input('enter element')
+    list_1.append(input_1)
 print('enter element for list 2:::')
-for k in range(0,d):
-    f=input('enter element')
-    b.append(f)
+for k in range(0,num_2):
+    input_2=input('enter element')
+    list_2.append(input_2)
 
-for i in range(0,d):
+for i in range(0,num_2):
     
-    c[a[i]]=b[i]
-print('your dictionary is:::',c)    
+    dict_1[list_1[i]]=list_2[i]
+print('your dictionary is:::',dict_1)    
